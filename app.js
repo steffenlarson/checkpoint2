a99c4d18c98440369c543f0e67a9ef2641903971
Steffen Larson <steffman13@gmail.com>
2020-12-13 19:35:07 -0700

    plugging in variables

--- app.js
+++ app.js
@@ -37,11 +37,31 @@ function update() {
     console.log("Herro dere")
     document.getElementById('cat-number').innerHTML = `${totalCats}`
 
+    // Need help here. Trying to input the value of the price onto the buttons. That variable is changing.
+    // connot read property price of undefined
+    let treatPrice = clickUpgrades.treat.price;
+    let fishPrice = autoUpgrades.fishOnALine.price;
+    let ladyPrice = autoUpgrades.crazyCatLady.price;
+    let laserPrice = clickUpgrades.laser.price
 
-    document.getElementById('treat-price').innerHTML = `${VARIABLE}`
-    document.getElementById('fish-price').innerHTML = `${VARIABLE}`
-    document.getElementById('lady-price').innerHTML = `${VARIABLE}`
-    document.getElementById('laser-price').innerHTML = `${VARIABLE}`
+
+    document.getElementById('treat-price').innerHTML = `${treatPrice}`
+    document.getElementById('fish-price').innerHTML = `${fishPrice}`
+    document.getElementById('lady-price').innerHTML = `${ladyPrice}`
+    document.getElementById('laser-price').innerHTML = `${laserPrice}`
+
+
+
+    let treatQuantity = clickUpgrades.treat.quantity;
+    let fishQuantity = autoUpgrades.fishOnALine.quantity;
+    let ladyQuantity = autoUpgrades.crazyCatLady.quantity;
+    let laserQuantity = clickUpgrades.laser.quantity
+
+
+    document.getElementById('treat-Quantity').innerHTML = ` X ${treatQuantity}`
+    document.getElementById('fish-Quantity').innerHTML = ` X ${fishQuantity}`
+    document.getElementById('lady-Quantity').innerHTML = ` X ${ladyQuantity}`
+    document.getElementById('laser-Quantity').innerHTML = ` X ${laserQuantity}`
 }
 
 function count() {
@@ -56,22 +76,22 @@ function buyUpgrade(userChoice) {
         && totalCats >= clickUpgrades.treats.price) {
         clickUpgrades.treats.quantity++
         totalCats -= clickUpgrades.treats.price
-        clickUpgrades.treats.price * .2
+        clickUpgrades.treats.price = clickUpgrades.treats.price * clickUpgrades.treats.multiplier
     } else if (userChoice == "laser"
         && totalCats >= clickUpgrades.laser.price) {
         clickUpgrades.laser.quantity++
         totalCats -= clickUpgrades.laser.price
-        clickUpgrades.laser.price * .2
+        clickUpgrades.laser.price = clickUpgrades.laser.price * clickUpgrades.laser.multiplier
     } else if (userChoice == "crazyCatLady"
         && totalCats >= autoUpgrades.crazyCatLady.price) {
         autoUpgrades.crazyCatLady.quantity++
         totalCats -= autoUpgrades.crazyCatLady.price
-        autoUpgrades.crazyCatLady.price * .2
+        autoUpgrades.crazyCatLady.price = autoUpgrades.crazyCatLady.price * autoUpgrades.crazyCatLady.multiplier
     } else if (userChoice == "fishOnALine"
         && totalCats >= autoUpgrades.fishOnALine.price) {
         autoUpgrades.fishOnALine.quantity++
         totalCats -= autoUpgrades.fishOnALine.price
-        autoUpgrades.fishOnALine.price * .2
+        autoUpgrades.fishOnALine.price = autoUpgrades.fishOnALine.price * autoUpgrades.fishOnALine.multiplier
     }
 
 
